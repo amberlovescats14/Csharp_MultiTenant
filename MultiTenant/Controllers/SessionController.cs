bbf78eaa681c6d846c25502ad9d3fbd0b14a281f
Amber Jones <amberlovescats14@gmail.com>
2020-06-20 16:27:29 -0500

    added controllers and about to download entity framework

---
+++ MultiTenant/Controllers/SessionController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultiTenant.Models;
+
+namespace MultiTenant.Controllers
+{
+    public class SessionController : Controller
+    {
+        public ActionResult Index()
+        {
+            var tenants = new List<Tenant>
+            {
+                new Tenant()
+                {
+                    Name = "SVCC",
+                    Domain = "www.siliconevalley-codecamp.com",
+                    Id = 1,
+                    Default = true
+                },
+                new Tenant()
+                {
+                    Name = "ANGU",
+                    Domain = "angularu.com",
+                    Id = 3,
+                    Default = false
+                },
+                new Tenant()
+                {
+                    Name = "CSSC",
+                    Domain = "codestarsummit.com",
+                    Id = 2,
+                    Default = false
+                }
+            };
+            return View(tenants);
+        }
+    }
+}
---
+++ MultiTenant/Models/Session.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultiTenant.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDesc { get; set; }
+        public string LongDesc { get; set; }
+        public Tenant Tenant { get; set; }
+        public List<Speaker> Speakers { get; set; }
+
+        public Session()
+        {
+        }
+    }
+}
